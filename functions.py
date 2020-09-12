0c17857db8c62bf8334ff56463259725a6385d78
souzalm <souzalm@gmail.com>
2020-09-12 16:57:25 -0300

    Atualização do corpo do código.
    Essa solução como está ainda não consegue avaliar a alteração dos states. Desenvolvi quatro classes. Sendo a classe context Tela(). E as classes com os gatilhos (métodos para as 6 ações diferentes) são Til(), Ti() e Tl(). Para a execução do código, rodar o arquivo main.py e indicar os caminhos dos arquivos (de entrada e de saída) com o nome do arquivo e seu tipo, no caso .txt. Existe a função tela() que ao ser executada realiza o comportamento para as transições das telas. O problema dessa solução está no fato de que o state do objeto instanciado de Tela() não está alterando dentro da execução dos métodos (gatilhos) que apontam para a instanciação de um objeto com valor diferente na mesma variável "t". Acredito que não seja possível sobrescrever objetos ou atributos em objetos. Encontrei algo na internet falando de uma função específica para isso, mas que não é recomendado. Acredito que devido ao fato de serem criados espaços na memória diferentes com a mesma atribuição.

--- functions.py
+++ functions.py
@@ -1,9 +1,18 @@
+class Tela():
+
+    def __init__(self, state):
+        self.state = state
+
+    def setstatus(self):
+        return self.state
+
+
 def entrada():
     caminho = input('Digite o caminho do arquivo .txt a ser aberto: ')
     return caminho
 
 
-def saida():
+def arquivosaida():
     caminho = input('Digite o caminho do arquivo .txt a ser escrito: ')
     return caminho
 
@@ -14,158 +23,172 @@ def ler_txt(path):
     return txt
 
 
-def iterador(entrada):
-    t = Tela(1)
-    for linha in entrada:
-        linha = linha.rstrip('\n')
-        tela(linha, t.setstatus())
-
-
 def escrever_txt(path):
     saida = open(path, 'w')
     return saida
 
 
-def tela(action, tela):
+def tela(action, tela, saida):
 
-    til = Til()
-    ti = Ti()
-    tl = Tl()
+    til = Til(1, 'None', 'None')
+    ti = Ti(2, 'None', 'None')
+    tl = Tl(3, 'None', 'None')
 
-    if til.setstatus() = tela:
-        til(1, action)
+    if til.state == tela:
+        til = Til(1, action, saida)
+        til.pfc()
+        til.pna()
+        til.bd()
+        til.pqr()
+        til.ad()
+        til.bl()
 
-    if ti.setstatus() = tela:
-        til(2, action)
+    if ti.state == tela:
+        ti = Til(2, action, saida)
+        ti.pfc()
+        ti.pna()
+        ti.bd()
+        ti.pqr()
+        ti.ad()
+        ti.bl()
 
-    if tl.setstatus() = tela:
-        til(3, action)
-
-
-class Tela():
-
-    def __init__(self, state):
-        self.state = state
-
-    def setstatus(self):
-        return self.state
+    if tl.state == tela:
+        tl = Til(3, action, saida)
+        tl.pfc()
+        tl.pna()
+        tl.bd()
+        tl.pqr()
+        tl.ad()
+        tl.bl()
 
 
 class Til():
 
-    def __init__(self, state, action):
-        self.state = None
-        self.action = None
+    def __init__(self, state, action, saida):
+        self.state = state
+        self.action = action
+        self.saida = saida
 
     def setstatus(self):
         return self.state
 
     def setinput(self):
-        return self.input
+        return self.action
+
+    def setsaida(self):
+        return self.saida
 
     def pfc(self):
-        if self.action = "PFC" or self.action = "pfc" or self.action = "Pfc" or self.action = "PFc" or self.action = "pfC" or self.action = "pFc":
-            saida.append('Não se aplica!')
+        if self.action == "PFC" or self.action == "pfc" or self.action == "Pfc" or self.action == "PFc" or self.action == "pfC" or self.action == "pFc":
+            self.saida.write('\nNao se aplica!')
 
     def pna(self):
-        if self.action = "PNA" or self.action = "pna" or self.action = "Pna" or self.action = "PNa" or self.action = "pnA" or self.action = "pNa":
-            saida.append('Não se aplica!')
+        if self.action == "PNA" or self.action == "pna" or self.action == "Pna" or self.action == "PNa" or self.action == "pnA" or self.action == "pNa":
+            self.saida.write('\nNao se aplica!')
 
     def bd(self):
-        if self.action = "BD" or self.action = "db" or self.action = "Bd" or self.action = "bD":
-            saida.append('Não se aplica!')
+        if self.action == "BD" or self.action == "db" or self.action == "Bd" or self.action == "bD":
+            self.saida.write('\nNao se aplica!')
 
     def pqr(self):
-        if self.action = "PQR" or self.action = "pqr" or self.action = "Pqr" or self.action = "PQr" or self.action = "pqR" or self.action = "pQr":
-            saida.append('Não se aplica!')
+        if self.action == "PQR" or self.action == "pqr" or self.action == "Pqr" or self.action == "PQr" or self.action == "pqR" or self.action == "pQr":
+            self.saida.write('\nNao se aplica!')
 
     def ad(self):
-        if self.action = "AD" or self.action = "ad" or self.action = "Ad" or self.action = "aD":
-            saida.append('Não se aplica!')
+        if self.action == "AD" or self.action == "ad" or self.action == "Ad" or self.action == "aD":
+            self.saida.write('\nNao se aplica!')
 
     def bl(self):
-        if self.action = "BL" or self.action = "bl" or self.action = "Bl" or self.action = "bL":
-            saida.append('TI')
-            t = tela(2)
+        if self.action == "BL" or self.action == "bl" or self.action == "Bl" or self.action == "bL":
+            self.saida.write('\nTI')
+            t = Tela(2)
 
 
 class Ti():
 
-    def __init__(self, state, action):
-        self.state = None
-        self.action = None
+    def __init__(self, state, action, saida):
+        self.state = state
+        self.action = action
+        self.saida = saida
 
     def setstatus(self):
         return self.state
 
     def setinput(self):
-        return self.input
+        return self.action
+
+    def setsaida(self):
+        return self.saida
 
     def pfc(self):
-        if self.action = "PFC" or self.action = "pfc" or self.action = "Pfc" or self.action = "PFc" or self.action = "pfC" or self.action = "pFc":
-            saida.append('Não se aplica!')
+        if self.action == "PFC" or self.action == "pfc" or self.action == "Pfc" or self.action == "PFc" or self.action == "pfC" or self.action == "pFc":
+            self.saida.write('\nNao se aplica!')
 
     def pna(self):
-        if self.action = "PNA" or self.action = "pna" or self.action = "Pna" or self.action = "PNa" or self.action = "pnA" or self.action = "pNa":
-            saida.append('TIL')
-            t = tela(2)
+        if self.action == "PNA" or self.action == "pna" or self.action == "Pna" or self.action == "PNa" or self.action == "pnA" or self.action == "pNa":
+            self.saida.write('\nTIL')
+            t = Tela(2)
 
     def bd(self):
-        if self.action = "BD" or self.action = "db" or self.action = "Bd" or self.action = "bD":
-            saida.append('TIL')
-            t = tela(2)
+        if self.action == "BD" or self.action == "db" or self.action == "Bd" or self.action == "bD":
+            self.saida.write('\nTIL')
+            t = Tela(2)
 
     def pqr(self):
-        if self.action = "PQR" or self.action = "pqr" or self.action = "Pqr" or self.action = "PQr" or self.action = "pqR" or self.action = "pQr":
-            saida.append('TIL')
-            t = tela(2)
+        if self.action == "PQR" or self.action == "pqr" or self.action == "Pqr" or self.action == "PQr" or self.action == "pqR" or self.action == "pQr":
+            self.saida.write('\nNao se aplica!')
+            t = Tela(2)
 
     def ad(self):
-        if self.action = "AD" or self.action = "ad" or self.action = "Ad" or self.action = "aD":
-            saida.append('TL')
-            t = tela(3)
+        if self.action == "AD" or self.action == "ad" or self.action == "Ad" or self.action == "aD":
+            self.saida.write('\nTL')
+            t = Tela(3)
 
     def bl(self):
-        if self.action = "BL" or self.action = "bl" or self.action = "Bl" or self.action = "bL":
-            saida.append('Não se aplica!')
+        if self.action == "BL" or self.action == "bl" or self.action == "Bl" or self.action == "bL":
+            self.saida.write('\nNao se aplica!')
 
 
 class Tl():
 
-    def __init__(self, state, action):
-        self.state = None
-        self.action = None
+    def __init__(self, state, action, saida):
+        self.state = state
+        self.action = action
+        self.saida = saida
 
     def setstatus(self):
         return self.state
 
     def setinput(self):
-        return self.input
+        return self.action
+
+    def setsaida(self):
+        return self.saida
 
     def pfc(self):
-        if self.action = "PFC" or self.action = "pfc" or self.action = "Pfc" or self.action = "PFc" or self.action = "pfC" or self.action = "pFc":
-            saida.append('TIL')
-            t = tela(2)
+        if self.action == "PFC" or self.action == "pfc" or self.action == "Pfc" or self.action == "PFc" or self.action == "pfC" or self.action == "pFc":
+            self.saida.write('\nTIL')
+            t = Tela(2)
 
     def pna(self):
-        if self.action = "PNA" or self.action = "pna" or self.action = "Pna" or self.action = "PNa" or self.action = "pnA" or self.action = "pNa":
-            saida.append('TIL')
-            t = tela(2)
+        if self.action == "PNA" or self.action == "pna" or self.action == "Pna" or self.action == "PNa" or self.action == "pnA" or self.action == "pNa":
+            self.saida.write('\nTIL')
+            t = Tela(2)
 
     def bd(self):
-        if self.action = "BD" or self.action = "db" or self.action = "Bd" or self.action = "bD":
-            saida.append('TIL')
-            t = tela(2)
+        if self.action == "BD" or self.action == "db" or self.action == "Bd" or self.action == "bD":
+            self.saida.write('\nTIL')
+            t = Tela(2)
 
     def pqr(self):
-        if self.action = "PQR" or self.action = "pqr" or self.action = "Pqr" or self.action = "PQr" or self.action = "pqR" or self.action = "pQr":
-            saida.append('TIL')
-            t = tela(2)
+        if self.action == "PQR" or self.action == "pqr" or self.action == "Pqr" or self.action == "PQr" or self.action == "pqR" or self.action == "pQr":
+            self.saida.write('\nTIL')
+            t = Tela(2)
 
     def ad(self):
-        if self.action = "AD" or self.action = "ad" or self.action = "Ad" or self.action = "aD":
-            saida.append('Não se aplica!')
+        if self.action == "AD" or self.action == "ad" or self.action == "Ad" or self.action == "aD":
+            self.saida.write('\nNao se aplica!')
 
     def bl(self):
-        if self.action = "BL" or self.action = "bl" or self.action = "Bl" or self.action = "bL":
-            saida.append('Não se aplica!')
+        if self.action == "BL" or self.action == "bl" or self.action == "Bl" or self.action == "bL":
+            self.saida.write('\nNao se aplica!')
